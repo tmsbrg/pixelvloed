9d3bed6413f328a80a03c614bce9a2e3a0632a31
Thomas van der Berg <ik@thomasvanderberg.nl>
2017-07-01 17:42:59 +0200

    Add ColorLine, DrawImage & PrepareImage

--- client.py
+++ client.py
@@ -5,11 +5,13 @@ Inspired by the PixelFlut projector on eth0:winter 2016 and
 code from https://github.com/defnull/pixelflut/
 """
 
-__version__ = 0.3
-__author__ = "Jan Klopper <jan@underdark.nl>"
+__version__ = 0.3001
+__author__ = "Thomas van der Berg <ik@thomasvanderberg.nl>"
 
 import random
-from vloed import PixelVloedClient, Packet, RGBPixel, MAX_PIXELS
+from PIL import Image
+import cv
+from vloed import *
 
 def RandomFill(pixels, width, height):
   """Generates a random number of pixels with a random color"""
@@ -20,6 +22,44 @@ def RandomFill(pixels, width, height):
                      random.randint(0, 255),
                      random.randint(0, 255))
     pixels.append(pixel)
+
+def ColorLine(pixels, width, y, r, g, b):
+  """Generates a random number of pixels with a random color"""
+  for pixel in xrange(0, random.randint(10, MAX_PIXELS)):
+    pixel = RGBPixel(random.randint(0, width), y, r, g, b)
+    pixels.append(pixel)
+
+def DrawImage(image, pixels):
+    width = image.size[0]
+    height = image.size[1]
+    for y in xrange(0, height):
+        for x in xrange(0, width):
+            rgb = image.getpixel((x, y))
+            pixel = RGBPixel(x, y, rgb[0], rgb[1], rgb[2])
+            pixels.append(pixel)
+
+prepared_image_packets = []
+
+def PrepareImage(image):
+    image_pixels = []
+    width = image.size[0]
+    height = image.size[1]
+    for y in xrange(0, height):
+        for x in xrange(0, width):
+            rgb = image.getpixel((x, y))
+            pixel = RGBPixel(x, y, rgb[0], rgb[1], rgb[2])
+            image_pixels.append(pixel)
+    random.shuffle(image_pixels)
+    for packet_start in xrange(0, len(image_pixels), MAX_PIXELS):
+        msg = []
+        InitMessage(msg)
+        packet_end = packet_start + MAX_PIXELS
+        msg += image_pixels[packet_start:packet_end]
+        prepared_image_packets.append(''.join(msg))
+
+def DrawPreparedImage(client):
+    for packet in prepared_image_packets:
+        client.SendPacket(packet)
 
 def RunClient(options):
   """Discover the servers and start sending to the first one"""
@@ -32,18 +72,24 @@ def RunClient(options):
                             options.height  # Screen pixels height, None for autodetect
                             )
 
+  image = Image.open("fracklogo.png")
+  PrepareImage(image)
+
+  # cap = cv.CaptureFromCAM(0)
+
   # loop the effect until we cancel by pressing ctrl+c / exit the program
   while True:
+    # frame = cv.QueryFrame(cap)
 
     # packet will automatically send its pixels if gets to the maximum pixel length
-    pixels = Packet(client)
+    #pixels = Packet(client)
 
     # add some pixels to the output with our functions
     # the width/height are read from the client's config
-    RandomFill(pixels, client.width, client.height)
+    DrawPreparedImage(client)
 
     # send whatever pixels are left in the packet
-    pixels.flush()
+    #pixels.flush()
 
 
 if __name__ == '__main__':
